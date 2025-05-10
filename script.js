5653898e57733868f6b4f6ce9bc8c3d238fab55c
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2025-05-10 12:57:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let slider = document.querySelector('#slider')
+let indicators
+let indicatorList = document.querySelectorAll('#indicatorList')
+let images = [
+    'i (1).webp', 'i (2).webp', 'i.webp'
+]
+for (let i = 0; i < images.length; i++) {
+    let indicator = document.createElement('li')
+    let indicatorList = document.appendChild(indicator)
+
+}
